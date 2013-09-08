fba44d65b9823a65c53a3b1bde3bec0e2741cec0
Teemu Matilainen <teemu.matilainen@iki.fi>
2013-09-08 00:32:23 -0300

    Set default proxy configuration for Chef provisioners
    Set proxy settings for all Chef provisioners based on
    `config.proxy.http` etc. The provisioner specific configuration
    still takes precedence.
    
    Chef configuration wants the (optional) username and password in
    separate config keys, so we need to extract them from our URI.

---
+++ lib/vagrant-proxyconf/action/configure_chef_proxy.rb
@@ -0,0 +1,65 @@
+module VagrantPlugins
+  module ProxyConf
+    class Action
+      # Action for configuring Chef provisioners
+      class ConfigureChefProxy
+        attr_reader :logger
+
+        def initialize(app, env)
+          @app    = app
+          @logger = Log4r::Logger.new('vagrant::proxyconf')
+        end
+
+        def call(env)
+          @app.call env
+
+          machine = env[:machine]
+          config  = config(machine)
+
+          if chef_provisioners(machine).empty?
+            logger.debug I18n.t("vagrant_proxyconf.chef_proxy.no_provisioners")
+          elsif !config.enabled?
+            logger.debug I18n.t("vagrant_proxyconf.chef_proxy.not_enabled")
+          else
+            env[:ui].info I18n.t("vagrant_proxyconf.chef_proxy.configuring")
+            configure_chef_provisioners(machine, config)
+          end
+        end
+
+        private
+
+        def config(machine)
+          config = machine.config.proxy
+          config.finalize! if Gem::Version.new(Vagrant::VERSION) < Gem::Version.new('1.2.5')
+          config
+        end
+
+        # @return [Array] all Chef provisioners
+        def chef_provisioners(machine)
+          machine.config.vm.provisioners.select { |prov| [:chef_solo, :chef_client].include?(prov.name) }
+        end
+
+        # Configures all Chef provisioner based on the default config
+        def configure_chef_provisioners(machine, config)
+          chef_provisioners(machine).each { |prov| configure_chef(prov.config, config) }
+        end
+
+        def configure_chef(chef, config)
+          if chef.http_proxy.nil?
+            chef.http_proxy      = config.http
+            chef.http_proxy_user = config.http_user
+            chef.http_proxy_pass = config.http_pass
+          end
+          if chef.https_proxy.nil?
+            chef.https_proxy      = config.https
+            chef.https_proxy_user = config.https_user
+            chef.https_proxy_pass = config.https_pass
+          end
+          if chef.no_proxy.nil?
+            chef.no_proxy = config.no_proxy
+          end
+        end
+      end
+    end
+  end
+end
