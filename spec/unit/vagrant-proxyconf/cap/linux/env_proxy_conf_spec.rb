5f3aa214c8391cbe088d1542cd13347e9a2731cc
Teemu Matilainen <teemu.matilainen@iki.fi>
2013-09-10 18:25:29 -0300

    Add unit test for Cap::Linux::EnvProxyConf.env_proxy_conf

---
+++ spec/unit/vagrant-proxyconf/cap/linux/env_proxy_conf_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+require 'vagrant-proxyconf/cap/linux/env_proxy_conf'
+
+describe VagrantPlugins::ProxyConf::Cap::Linux::EnvProxyConf do
+
+  describe '.env_proxy_conf' do
+    let(:subject) { described_class.env_proxy_conf(double) }
+    it { should eq '/etc/profile.d/proxy.sh' }
+  end
+
+end
