e742a26c864652735ca7dfd85edc73c0ce59fa1d
Teemu Matilainen <teemu.matilainen@iki.fi>
2014-02-17 23:40:41 -0300

    Add support for npm proxy
    Configure npm to use proxies if it is installed.
    Hook also after each provisioner run.
    
    Fixes #50

---
+++ lib/vagrant-proxyconf/cap/linux/npm_proxy_conf.rb
@@ -0,0 +1,15 @@
+module VagrantPlugins
+  module ProxyConf
+    module Cap
+      module Linux
+        # Capability for npm proxy configuration
+        module NpmProxyConf
+          # @return [Boolean] if npm is installed
+          def self.npm_proxy_conf(machine)
+            machine.communicate.test('which npm')
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/unit/vagrant-proxyconf/cap/linux/npm_proxy_conf_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+require 'vagrant-proxyconf/cap/linux/npm_proxy_conf'
+
+describe VagrantPlugins::ProxyConf::Cap::Linux::NpmProxyConf do
+
+  describe '.npm_proxy_conf' do
+    let(:machine) { double }
+    let(:communicator) { double }
+
+    before do
+      machine.stub(:communicate => communicator)
+    end
+
+    it "returns true when npm is installed" do
+      expect(communicator).to receive(:test).with('which npm').and_return(true)
+      expect(described_class.npm_proxy_conf(machine)).to be_true
+    end
+
+    it "returns false when npm is not installed" do
+      expect(communicator).to receive(:test).with('which npm').and_return(false)
+      expect(described_class.npm_proxy_conf(machine)).to be_false
+    end
+  end
+
+end
