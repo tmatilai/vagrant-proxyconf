69fd37b7ba215c6543d72942a479a638468e225e
Teemu Matilainen <teemu.matilainen@iki.fi>
2014-04-06 23:43:08 +0300

    Use absolute path to `git` when configuring it

--- lib/vagrant-proxyconf/cap/linux/git_proxy_conf.rb
+++ lib/vagrant-proxyconf/cap/linux/git_proxy_conf.rb
@@ -1,11 +1,14 @@
+require_relative '../util'
+
 module VagrantPlugins
   module ProxyConf
     module Cap
       module Linux
-        # Capability for Git command
+        # Capability for git proxy configuration
         module GitProxyConf
+          # @return [String, false] the path to git or `false` if not found
           def self.git_proxy_conf(machine)
-            machine.communicate.test('which git')
+            Util.which(machine, 'git')
           end
         end
       end
