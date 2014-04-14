55d072a4a6f95437f17107f86cc4f34cac47bb1d
James Lawson <james.lawson@comparethemarket.com>
2014-04-14 13:19:52 +0100

    Add support for configuring environment variables on Windows guests
    Signed-off-by: Teemu Matilainen <teemu.matilainen@iki.fi>

--- lib/vagrant-proxyconf/action/configure_env_proxy.rb
+++ lib/vagrant-proxyconf/action/configure_env_proxy.rb
@@ -12,8 +12,21 @@ module VagrantPlugins
         private
 
         def configure_machine
-          super
-          write_config(sudo_config, path: '/etc/sudoers.d/proxy', mode: '0440')
+          if windows_guest?
+            logger.info('Setting the Windows proxy environment variables')
+            configure_machine_windows
+          else
+            logger.info('Writing the proxy configuration to files')
+            super
+            write_config(sudo_config, path: '/etc/sudoers.d/proxy', mode: '0440')
+          end
+        end
+
+        def configure_machine_windows
+          set_windows_proxy('http_proxy', config.http)
+          set_windows_proxy('https_proxy', config.https)
+          set_windows_proxy('ftp_proxy', config.ftp)
+          set_windows_proxy('no_proxy', "\"#{config.no_proxy}\"")
         end
 
         def sudo_config
@@ -22,6 +35,21 @@ module VagrantPlugins
             Defaults env_keep += "http_proxy https_proxy ftp_proxy no_proxy"
           CONFIG
         end
+
+        def set_windows_proxy(key, value)
+          if value
+            command = "cmd.exe /c SETX #{key} #{value} /M"
+            logger.info("Setting #{key} to #{value}")
+            @machine.communicate.sudo(command)
+          else
+            logger.info("Not setting #{key}")
+          end
+        end
+
+        def windows_guest?
+          @machine.config.vm.guest.eql?(:windows)
+        end
+
       end
     end
   end
