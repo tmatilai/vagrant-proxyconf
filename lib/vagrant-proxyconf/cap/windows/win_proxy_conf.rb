36240824079690968b9d1549ba9d3067326739b6
James Lawson <james.lawson@comparethemarket.com>
2014-04-23 08:33:18 +0100

    Added Windows capability

--- lib/vagrant-proxyconf/cap/windows/win_proxy_conf.rb
+++ lib/vagrant-proxyconf/cap/windows/win_proxy_conf.rb
@@ -4,7 +4,7 @@ module VagrantPlugins
   module ProxyConf
     module Cap
       module Windows
-        # Capability for git proxy configuration
+        # Capability for windows host proxy configuration
         module WinProxyConf
           # @return [String, false]
           def self.win_proxy_conf(machine)
