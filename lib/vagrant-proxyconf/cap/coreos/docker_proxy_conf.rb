a7110ea72fbb59c488306a06b869296e782734a4
Teemu Matilainen <teemu.matilainen@iki.fi>
2014-11-30 23:12:40 +0200

    Style nitpick

--- lib/vagrant-proxyconf/cap/coreos/docker_proxy_conf.rb
+++ lib/vagrant-proxyconf/cap/coreos/docker_proxy_conf.rb
@@ -11,10 +11,10 @@ module VagrantPlugins
             return false unless Util.which(machine, 'docker')
 
             machine.communicate.tap do |comm|
-              src_file='/usr/lib/systemd/system/docker.service'
-              dst_file='/etc/systemd/system/docker.service'
-              tmp_file='/tmp/docker.service'
-              env_file='EnvironmentFile=-\/etc\/default\/docker'
+              src_file = '/usr/lib/systemd/system/docker.service'
+              dst_file = '/etc/systemd/system/docker.service'
+              tmp_file = '/tmp/docker.service'
+              env_file = 'EnvironmentFile=-\/etc\/default\/docker'
               comm.sudo("sed -e 's/\\[Service\\]/[Service]\\n#{env_file}/g' #{src_file} > #{tmp_file}")
               unless comm.test("diff #{tmp_file} #{dst_file}")
                 # update config and restart docker when config changed
