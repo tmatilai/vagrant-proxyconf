11bdd60551319630b0c72b9f922d1024a62f710e
Hiroshi Ota <otahi.pub@gmail.com>
2015-10-11 07:56:37 +0900

    Refator systemd proxy environment variables reflection

--- lib/vagrant-proxyconf/action/configure_systemd_proxy.rb
+++ lib/vagrant-proxyconf/action/configure_systemd_proxy.rb
@@ -7,6 +7,10 @@ module VagrantPlugins
     class Action
       # Action for configuring systemd on the guest
       class ConfigureSystemdProxy < Base
+        TMP_PATH = '/tmp/vagrant-proxyconf'
+        CONFIG_REGEXP = '^DefaultEnvironment.*#vagrant-proxyconf'
+        I18N_PREFIX = 'vagrant_proxyconf.systemd_proxy.'
+
         def config_name
           'systemd_proxy'
         end
@@ -21,38 +25,45 @@ module VagrantPlugins
         def configure_machine
           logger.info('Writing the proxy configuration to systemd config')
           write_systemd_config
+          reflect_config
         end
 
         def write_systemd_config
-          path = config_path
+          env_config =
+            "DefaultEnvironment=#{systemd_env_settings} #vagrant-proxyconf"
 
           @machine.communicate.tap do |comm|
-            if (comm.sudo(" systemctl show-environment | grep -c \"http_proxy=#{config.http || ''}\"").to_i rescue 0) == 0
-              comm.sudo("echo -e '#{systemd_env_settings}' >> #{path}")
-              comm.sudo(service_restart_command_low)
-              comm.sudo(service_restart_command_up)
-            end
+            comm.sudo("cp -p #{config_path} #{TMP_PATH}", error_check: false)
+            comm.sudo("grep -ve '#{CONFIG_REGEXP}' #{config_path} > #{TMP_PATH}")
+            comm.sudo("echo -e '#{env_config}' >> #{TMP_PATH}")
+            @restart_needed = !comm.test("diff -w #{TMP_PATH} #{config_path}")
+            comm.sudo("mv '#{TMP_PATH}' #{config_path}")
           end
         end
 
-        def service_restart_command_up
-          "systemctl set-environment \"HTTP_PROXY=#{config.http || ''}\" && " +
-            "systemctl set-environment \"HTTPS_PROXY=#{config.https || ''}\" && " +
-            "systemctl set-environment \"NO_PROXY=#{config.no_proxy || ''}\""
-        end
+        def reflect_config
+          return unless @restart_needed
+          @machine.ui.info(I18n.t("#{I18N_PREFIX}restarting_guest_start"))
+          @machine.communicate.sudo('shutdown -r now')
 
-        def service_restart_command_low
-          "systemctl set-environment \"http_proxy=#{config.http || ''}\" && " +
-            "systemctl set-environment \"https_proxy=#{config.https || ''}\" && " +
-            "systemctl set-environment \"no_proxy=#{config.no_proxy || ''}\" "
+          sleep 5 until @machine.communicate.ready?
+          @machine.ui.info(I18n.t("#{I18N_PREFIX}restarting_guest_done"))
         end
 
         def systemd_env_settings
-          "DefaultEnvironment=\"https_proxy=#{config.http || ''}\" "+
-            "\"http_proxy=#{config.http || ''}\" "+
-            "\"no_proxy=#{config.no_proxy || ''}\""
+          https_proxy = config.https    || ''
+          http_proxy  = config.http     || ''
+          ftp_proxy   = config.ftp      || ''
+          no_proxy    = config.no_proxy || ''
+          ["HTTPS_PROXY=#{https_proxy}",
+            "HTTP_PROXY=#{http_proxy}",
+            "FTP_PROXY=#{ftp_proxy}",
+            "NO_PROXY=#{no_proxy}",
+            "https_proxy=#{https_proxy}",
+            "http_proxy=#{http_proxy}",
+            "ftp_proxy=#{ftp_proxy}",
+            "no_proxy=#{no_proxy}"].join(' ')
         end
-
       end
     end
   end
